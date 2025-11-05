53ad3e6010dcc35e784b6c710083a54f95fdda0b
Mehdi Bigdeli <bigdeli.me@raikaco.org>
2025-11-05 13:26:59 +0330

    foo

---
+++ ezwsc.go
@@ -0,0 +1,97 @@
+package ezwsc
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/crypto4people/zoro/singbox"
+
+	libbox "github.com/sagernet/sing-box/experimental/libbox"
+)
+
+var (
+	mu          sync.Mutex
+	svc         *libbox.BoxService
+	cancelGuard context.CancelFunc
+)
+
+func IsRunning() bool {
+	mu.Lock()
+	defer mu.Unlock()
+
+	return svc != nil
+}
+
+func parseKey(privateKeyHex string) (*ecdsa.PrivateKey, error) {
+	hex := strings.TrimPrefix(strings.TrimSpace(privateKeyHex), "0x")
+	return crypto.HexToECDSA(hex)
+}
+
+func StartWithTunFD(ctx context.Context, fd int, server string, privateKeyHex string) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if svc != nil {
+		return errors.New("vpn already running")
+	}
+	if fd < 0 {
+		return errors.New("invalid TUN fd")
+	}
+
+	priv, err := parseKey(privateKeyHex)
+	if err != nil {
+		return err
+	}
+
+	tunCIDR := "10.66.0.1/24"
+	sbConfig, err := singbox.Config(ctx, server, priv, &singbox.Settings{
+		TunAddr: &tunCIDR,
+	}, singbox.ModeTun)
+	if err != nil {
+		return err
+	}
+
+	confJSON, err := json.Marshal(sbConfig)
+	if err != nil {
+		return err
+	}
+
+	plat := &platform{fd: int32(fd)}
+	svc, err = libbox.NewService(string(confJSON), plat)
+	if err != nil {
+		return err
+	}
+
+	if err := svc.Start(); err != nil {
+		_ = svc.Close()
+		return err
+	}
+
+	if ctx != nil {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithCancel(ctx)
+		cancelGuard = cancel
+		go func() { <-ctx.Done(); _ = Stop() }()
+	}
+
+	return nil
+}
+
+func Stop() error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if svc == nil {
+		return nil
+	}
+	err := svc.Close()
+	svc = nil
+	cancelGuard = nil
+	return err
+}
